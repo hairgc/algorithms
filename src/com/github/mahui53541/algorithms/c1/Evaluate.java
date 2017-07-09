cd0a36dcdea76d85d673b0f14255d3d6cc669d95
mahui53541 <mahuiking9527@gmail.com>
2017-07-09 11:01:01 +0800

    add jar

---
+++ src/com/github/mahui53541/algorithms/c1/Evaluate.java
@@ -0,0 +1,24 @@
+package com.github.mahui53541.algorithms.c1;
+
+import java.util.Stack;
+
+import edu.princeton.cs.introcs.StdIn;
+
+/**   
+ * @ClassName:  Evaluate   
+ * @Description:算术表达式求值
+ * @author: mahui 
+ * @date:   2017年7月9日 上午10:42:27   
+ *     
+ */ 
+public class Evaluate { 
+	public static void main(String[] args) {
+		Stack<String> ops=new Stack<String>();
+		Stack<String> vals=new Stack<String>();
+		
+		while(!StdIn.isEmpty()) {
+			
+		}
+	}
+
+}
