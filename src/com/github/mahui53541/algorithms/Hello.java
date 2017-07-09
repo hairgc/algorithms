c0964f38bccf72fa2fdb073a26cf420c6490a4fb
mahui53541 <mahuiking9527@gmail.com>
2017-07-09 09:06:12 +0800

    创建项目

---
+++ src/com/github/mahui53541/algorithms/Hello.java
@@ -0,0 +1,10 @@
+package com.github.mahui53541.algorithms;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
